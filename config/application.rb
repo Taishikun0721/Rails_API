d420da6e48df15e98991a5ec29ada08b0304f511
Taishiro Inokuchi <tai0721abc@gmail.com>
2021-01-08 18:06:51 +0900

    :books: rails g で作成するファイルを制限

--- config/application.rb
+++ config/application.rb
@@ -10,10 +10,19 @@ module RailsApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.generators.system_tests = nil
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.assets false
+      g.skip_routes true
+      g.helper false
+      g.test_framework :rspec,
+                       view_specs: false,
+                       helper_spec: false,
+                       routing_spec: false
+    end
   end
 end
